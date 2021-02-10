40c7a450a87d47d6b9a94c4076af9e4286c3d875
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2021-02-10 23:42:51 +0100

    update

--- 08/exams/2019-lugl.ml
+++ 08/exams/2019-lugl.ml
@@ -1,6 +1,42 @@
 (* #use "08/exams/2019-lugl.ml";; *)
 
 (* ======================================================================== *)
+(* ================================== 1 =================================== *)
+(* ======================================================================== *)
+
+type 'a tree = Empty | Tr of 'a * 'a tree * 'a tree
+
+let root = function
+    | Empty -> failwith "root"
+    | Tr(x,_,_) -> x
+
+(* n_ramo_bin: int -> int tree -> int list *)
+(* n_ramo_bin n tree = cammino dalla root a una foglia tale che la somma di ogni nodo è uguale a n *)
+let rec n_ramo_bin n = function
+    | Empty -> failwith "n_ramo_bin"
+    | Tr(y,Empty,Empty) ->
+        if y = n then [y]
+        else failwith "n_ramo_bin"
+    | Tr(y,t,Empty) | Tr(y,Empty,t) ->
+        if (y + root t) <= n then y::(n_ramo_bin (n - y) t)
+        else failwith "n_ramo_bin"
+    | Tr(y,l,r) ->
+        try
+            if (y + root l) <= n then y::(n_ramo_bin (n - y) l)
+            else failwith "n_ramo_bin"
+        with _ -> 
+            if (y + root r) <= n then y::(n_ramo_bin (n - y) r)
+            else failwith "n_ramo_bin"
+
+let tree = Tr(
+    5,
+    Tr(7,Tr(3,Empty,Empty),Tr(9,Empty,Empty)),
+    Tr(3,Tr(7,Empty,Empty),Tr(10,Empty,Empty))
+)
+
+let result = n_ramo_bin 18 tree
+
+(* ======================================================================== *)
 (* ============== Luglio 2019, riformulato per alberi binari ============== *)
 (* ======================================================================== *)
 
